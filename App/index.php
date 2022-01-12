03caaf44a1432bc85d12a9ab17a306e977b3fa52
rodrigocastropilha@gmail.com <rodrigo castro>
2022-01-12 14:01:12 -0300

    imagen de fundo

--- App/index.php
+++ App/index.php
@@ -10,8 +10,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <style>
     body {
-
-      background-image: url("imgs/redimg.jpg");
+      background-image: url("carros.jpg");
       background-size: cover;
       background-repeat: no-repeat;
     }
--- App/resbusca.php
+++ App/resbusca.php
@@ -26,6 +26,10 @@ $dado = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>busca</title>
     <style>
+        body {
+            background-color: azure;
+        }
+
         h1 {
             text-align: center;
         }
